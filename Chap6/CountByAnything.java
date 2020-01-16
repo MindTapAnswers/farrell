6a775f021463a7fbdd1028a2f8fbc60c996a9b80
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-01-16 09:47:41 -0500

    Programming Exercise 6-1B

---
+++ Chap6/CountByAnything.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CountByAnything
+{
+   // Modify the code below
+   public static void main (String args[])
+   {
+       Scanner sc=new Scanner(System.in);
+System.out.println("Enter cash money number!!");
+int ben=sc.nextInt();
+      final int START = ben;
+      final int STOP = 500;
+      final int NUMBER_PER_LINE =10;
+      int count = 0;
+      for(int i = START; i <= STOP; i += START)
+      {
+         System.out.print(i + "  ");
+         count+=1;
+           if(count == 10) {
+  count = 0;
+  System.out.println();
+           }}}}
+
