8ca4c5c7b8df9682be29d0f2e9284841c8e15add
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-01-16 09:36:23 -0500

    Programming Exercise 6-1A

---
+++ Chap6/CountByFives.java
@@ -0,0 +1,11 @@
+public class CountByFives {
+    public static void main (String args[]) {
+        int number = 0;
+        while (number < 500) {
+            number += 5;
+            System.out.println(number);
+        }
+    }
+}
+
+
