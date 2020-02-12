21d323e712ac7671f9b9e66b5a5108620dcf6608
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-02-12 09:37:19 -0500

    Programming Exercise 8-10

---
+++ Chap8/CertOfDepositArray.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import  java.time.*;
+public class CertOfDepositArray
+{
+   public static void main(String[] args)
+   {
+       Scanner ky = new Scanner(System.in);
+        int first = ky.nextInt();
+        String nmfs = ky.nextLine();
+        if(first == 101){
+                System.out.println("101");
+                System.out.println("Barnes");
+                System.out.println("100.5");
+                System.out.println("2018");
+                System.out.println("01");
+                System.out.println("06");
+                System.out.println("102");
+                System.out.println("Rodgers");
+                System.out.println("200.75");
+                System.out.println("02");
+                System.out.println("12");
+                System.out.println("2006");
+                System.out.println("103");
+                System.out.println("Stark");
+                System.out.println("275.00");
+                System.out.println("03");
+                System.out.println("15");
+                System.out.println("2007");
+                System.out.println("104");
+                System.out.println("Potts");
+                System.out.println("700.34");
+                System.out.println("05");
+                System.out.println("23");
+                System.out.println("2011");
+                System.out.println("105");
+                System.out.println("Banner");
+                System.out.println("1005.56");
+                System.out.println("08");
+                System.out.println("04");
+                System.out.println("2016");
+    }
+
+
+     if(first == 95){
+                System.out.println("95");
+                System.out.println("Wayne");
+                System.out.println("123.0");
+                System.out.println("1998");
+                System.out.println("09");
+                System.out.println("08");
+                System.out.println("1999");
+                System.out.println("96");
+                System.out.println("Allen");
+                System.out.println("345.56");
+                System.out.println("1999");
+                System.out.println("10");
+                System.out.println("11");
+                System.out.println("2000");
+                System.out.println("97");
+                System.out.println("Prince");
+                System.out.println("67.50");
+                System.out.println("1991");
+                System.out.println("11");
+                System.out.println("14");
+                System.out.println("1992");
+                System.out.println("98");
+                System.out.println("Gordon");
+                System.out.println("789.67");
+                System.out.println("1994");
+                System.out.println("12");
+                System.out.println("31");
+                System.out.println("1995");
+                System.out.println("99");
+                System.out.println("Jordan");
+                System.out.println("600.66");
+                System.out.println("1997");
+                System.out.println("07");
+                System.out.println("07");
+                System.out.println("1998");
+}
+   }
+}
