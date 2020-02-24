8cc71393ba92d7b07df89ab6aa7b792e8d6edaed
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-02-24 09:23:15 -0500

    Programming Exercise 9-1

---
+++ Chap9/StringSort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class StringSort
+{
+   public static void main(String[] args)
+   {
+      String[] values =  {"mouse", "dog", "cat", "horse", "cow",
+         "moose", "tiger", "lion", "elephant", "bird", "hamster",
+         "guinea pig", "leopard", "aardvark", "hummingbird"}; 
+         int x;
+      String message = "Values:";
+      Arrays.sort(values);
+      for(x = 0; x < values.length; ++x)
+         message = message + "  " + values[x];
+      System.out.println(message);
+   }
+}
+
