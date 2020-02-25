2fa88e2f9c7f318ceaa851d553d4212d4c91fece
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-02-25 09:29:11 -0500

    Programming Exercise 9-3A

---
+++ Chap9/Recording.java
@@ -0,0 +1,29 @@
+public class Recording {
+    private String song;
+    private String artist;
+    private int playTime;
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getPlayTime() {
+        return playTime;
+    }
+
+    public void setPlayTime(int playTime) {
+        this.playTime = playTime;
+    }
+}
