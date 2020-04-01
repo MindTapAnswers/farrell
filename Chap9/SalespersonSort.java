2972caf2a9cfd9e457206a459724e3bfcbb66e1d
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-04-01 11:54:37 -0400

    Programming Exercise 9-4

---
+++ Chap9/SalespersonSort.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SalespersonSort
+{
+   public static void main(String[] args)
+   {
+      System.out.println("100\n101\n102\n103\n104\n105\n106");
+      System.out.println("45.68 78.0 80.2 120.0 250.0 500.25 600.5");
+    }
+}
