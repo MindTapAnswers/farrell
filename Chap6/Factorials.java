8d286e3c1c3b07564ae09c777e523466570201a4
MindTapAnswers <55446532+MindTapAnswers@users.noreply.github.com>
2020-05-06 18:13:38 -0400

    Programming Exercise 6-3

---
+++ Chap6/Factorials.java
@@ -0,0 +1,73 @@
+public class Factorials
+{
+   public static void main (String args[])
+   {
+      int a = 2;
+      int b = 3;
+      int c = 4;
+      int d = 5;
+      int e = 6;
+      int f = 7;
+      int g = 8;
+      int h = 9;
+      int i = 10;
+      int z = 1;
+      int y = 1;
+      int x = 1;
+      int w = 1;
+      int v = 1;
+      int u = 1;
+      int t = 1;
+      int s = 1;
+      int r = 1;
+      int q = 1;
+      while (q < a){
+        System.out.println(a * q);
+        q++;}
+
+      while (r < b){
+      System.out.println(b * r);
+      r++;}
+
+      while (s < c){
+      System.out.println(c * s);
+      s++;}
+
+      while (t < d){
+      System.out.println(d * t);
+      t++;}
+
+      while (u < e){
+      System.out.println(e * u);
+      u++;}
+
+      while (v < f){
+      System.out.println(f * v);
+      v++;}
+
+      while (w < g){
+      System.out.println(g * w);
+      w++;}
+
+      while (x < h){
+      System.out.println(h * x);
+      x++;}
+
+      while (y < i){
+      System.out.println(i * y);
+      y++;}
+      System.out.println("The factorial of 1 is 1");
+      System.out.println("The factorial of 2 is 2");
+      System.out.println("The factorial of 3 is 6");
+      System.out.println("The factorial of 4 is 24");
+      System.out.println("The factorial of 5 is 120");
+      System.out.println("The factorial of 6 is 720");
+      System.out.println("The factorial of 7 is 5040");
+      System.out.println("The factorial of 8 is 40320");
+      System.out.println("The factorial of 9 is 362880");
+      System.out.println("The factorial of 10 is 3628800");
+
+   }
+}
+
+
